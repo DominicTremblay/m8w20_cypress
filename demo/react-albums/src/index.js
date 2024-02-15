a368234100f568bd2bbb2c47bd9cfdc6df5a3a61
Dominic Tremblay <dominictremblay24@gmail.com>
2024-02-15 12:03:32 -0500

    removed react-albums

--- demo/react-albums/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./reset.css";
-import "./index.css";
-
-import LiveSearch from "components/LiveSearch";
-
-ReactDOM.render(<LiveSearch />, document.getElementById("root"));
